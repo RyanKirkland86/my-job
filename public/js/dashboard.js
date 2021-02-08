515c5930f03f7cb5ba07523b35e73739721f3f18
Jordan Kelly <profjjk@gmail.com>
2021-02-07 17:15:20 -0800

    consolidate styling into one master file

---
+++ public/js/dashboard.js
@@ -0,0 +1,13 @@
+$(document).ready(() => {
+  $(document.body).on('click', '.application', (event) => {
+    event.preventDefault();
+
+    const appId = event.target.getAttribute('data-id');
+    const userId; // how do I get req.params.id ??
+
+    console.log(appId)
+    console.log(userId)
+
+    window.location.pathname = (`/dashboard/1/${appId}`)
+  })
+})
--- routes/html-routes.js
+++ routes/html-routes.js
@@ -18,16 +18,32 @@ module.exports = function(app) {
   app.get("/dashboard/:id", async (req, res) => {
     const user = await db.User.findOne({
       where: { id: req.params.id }
-    });
+    }).catch(err => console.log(err));
     const apps = await db.Application.findAll({
-      where: { UserID: req.params.id }
-    })
+      where: { UserId: req.params.id }
+    }).catch(err => console.log(err));
     const data = {
       firstname: user.firstName,
       applications: apps
     }
     res.render("dashboard", data);
   });
+
+  app.get("/dashboard/:id/:appid", async (req, res) => {
+    const app = await db.Application.findOne({
+      where: { id: req.params.id }
+    }).catch(err => console.log(err));
+    const notes = await db.Note.findAll({
+      where: { ApplicationId: req.params.id }
+    }).catch(err => console.log(err));
+    const data = {
+      date: app.createdAt,
+      company: app.company,
+      role: app.role,
+      notes: notes
+    }
+    res.render("application", data);
+  })
 
   app.get("/dashboard/:id/:appid", isAuthenticated, function(req, res) {
     res.sendFile(path.join(__dirname, "../public/html/application.html"));
