a928222403327b286f5f1ef509278b29f317c470
Jordan Kelly <profjjk@gmail.com>
2021-02-08 11:59:39 -0800

    fix application page not loading

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -4,6 +4,7 @@ var db = require("../models")
 
 // Requiring our custom middleware for checking if a user is logged in
 var isAuthenticated = require("../config/middleware/isAuthenticated");
+const user = require("../models/user");
 
 module.exports = function(app) {
 
@@ -21,44 +22,59 @@ module.exports = function(app) {
   // app.get("/dashboard/:id", function(req, res) {
   //  res.sendFile(path.join(__dirname, "../public/html/dashboard.html"));
 
-  app.get("/dashboard/:id", (req, res) => {
-    db.User.findAll({
+  // app.get("/dashboard/:id", (req, res) => {
+  //   db.User.findAll({
+  //     where: { id: req.params.id },
+  //   }).then(function (result) {
+  //     console.log(result);
+  //     var applications = { firstName: result[0].dataValues.firstName };
+  //     db.Application.findAll({
+  //       where: { UserID: result[0].dataValues.id },
+  //     }).then(function (result) {
+  //       for (var i = 0; i < result.length; i++) {
+  //         applications.applications = result[i].dataValues;
+  //       }
+  //       res.render("dashboard", applications);
+  //     });
+  //   });
+  // });
+
+  app.get("/dashboard/:id/", async (req, res) => {
+    const user = await db.User.findOne({
       where: { id: req.params.id }
-    }).then(function(result) {
-      console.log(result);
-      var applications = {firstName: result[0].dataValues.firstName};
-      db.Application.findAll({
-        where: { UserID: result[0].dataValues.id }
-      }).then(function(result) {
-        for(var i =0; i< result.length; i++) {
-          applications.applications = result[i].dataValues;
-        }
-        res.render("dashboard", applications);
-        });
-      });
-    });
-  
+    }).catch(err => console.log(err));
+    const apps = await db.Application.findAll({
+      where: { UserId: req.params.id }
+    }).catch(err => console.log(err));
+    const data = {
+      firstname: user.firstName,
+      applications: apps 
+    }
+    res.render("dashboard", data);
+  });
 
   app.get("/dashboard/:id/:appid", async (req, res) => {
     const app = await db.Application.findOne({
       where: { id: req.params.id }
     }).catch(err => console.log(err));
     const note = await db.Note.findAll({
-      where: { ApplicationId: req.params.id }
+      where: { ApplicationId: req.params.appid }
     }).catch(err => console.log(err));
-    console.log(app);
+    
+
     const data = {
       date: app.createdAt,
       company: app.company,
       role: app.role,
-      noteDate: note.createdAt,
-      noteBody: note.body
+      status: app.status,
+      notes: note
     }
+    console.log(data.notes);
     res.render("application", data);
   });
 
-  app.get("/dashboard/:id/:appid", isAuthenticated, function(req, res) {
-    res.sendFile(path.join(__dirname, "../public/html/application.html"));
-  });
+  // app.get("/dashboard/:id/:appid", isAuthenticated, function(req, res) {
+  //   res.sendFile(path.join(__dirname, "../public/html/application.html"));
+  // });
 
 };
