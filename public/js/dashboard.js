d37c676bb9ec7c61e9dcdec55aaad3829530d092
Jordan Kelly <profjjk@gmail.com>
2021-02-09 16:43:48 -0800

    add refresh button to dashboard

--- public/js/dashboard.js
+++ public/js/dashboard.js
@@ -1,4 +1,5 @@
 $(document).ready(function () {
+    // Create a new application.
     $("#save-app").on("click", function(event) {
         event.preventDefault();
         var compName = $("#compName").val().trim();
@@ -10,7 +11,6 @@ $(document).ready(function () {
         if (window.location.href.slice(-2, -1)==="/") {
             user = window.location.href.slice(-1);
         } else { user = window.location.href.slice(-2)};
-
 
         var newApp = {
             company: compName,
@@ -30,6 +30,7 @@ $(document).ready(function () {
         });     
     });
 
+    // Go to selected application's page.
     $(document.body).on("click", ".application", function (event) {
         event.preventDefault();
         var appId = event.target.getAttribute("data-id");
@@ -40,6 +41,7 @@ $(document).ready(function () {
         window.location.pathname = (`/dashboard/${user}/${appId}`);
     })
 
+    // Search for applications by company name.
     $(document.body).on("click", "#btn-search", event => {
         event.preventDefault();
         $('#applications').empty();
@@ -50,24 +52,29 @@ $(document).ready(function () {
         $.get(`/api/applications/company/${userID}`, inputData, data => {
             for (var i = 0; i < data.length; i++) {
                 var appBlock = `
-            <div class="row mt-2 application color-background hover-dark imitate-btn rounded-border" data-id="${data[i].id}">
-            <div class="col-2 py-2" data-id="${data[i].id}">
-                <!-- Need to figure out how to format the 'createdAt' date -->
-                <p class="text-center m-0 light" data-id="${data[i].id}">${dayjs(data[i].createdAt).format('MMM DD YYYY')}</p>
-            </div>
-            <div class="col-4 py-2" data-id="${data[i].id}">
-                <p class="text-center m-0 light" data-id="${data[i].id}">${data[i].company}</p>
-            </div>
-            <div class="col-4 py-2" data-id="${data[i].id}">
-                <p class="text-center m-0 light" data-id="${data[i].id}">${data[i].role}</p>
-            </div>
-            <div class="col-2 py-2" data-id="${data[i].id}">
-                <!-- Need a 'source' column on the application table -->
-                <p class="text-center m-0 light" data-id="${data[i].id}">${data[i].jobsitelink}</p>
-            </div>
-            </div>`
-            $('#applications').append(appBlock)
+                <div class="row mt-2 application color-background hover-dark imitate-btn rounded-border" data-id="${data[i].id}">
+                <div class="col-2 py-2" data-id="${data[i].id}">
+                    <!-- Need to figure out how to format the 'createdAt' date -->
+                    <p class="text-center m-0 light" data-id="${data[i].id}">${dayjs(data[i].createdAt).format('MMM DD YYYY')}</p>
+                </div>
+                <div class="col-4 py-2" data-id="${data[i].id}">
+                    <p class="text-center m-0 light" data-id="${data[i].id}">${data[i].company}</p>
+                </div>
+                <div class="col-4 py-2" data-id="${data[i].id}">
+                    <p class="text-center m-0 light" data-id="${data[i].id}">${data[i].role}</p>
+                </div>
+                <div class="col-2 py-2" data-id="${data[i].id}">
+                    <!-- Need a 'source' column on the application table -->
+                    <p class="text-center m-0 light" data-id="${data[i].id}">${data[i].jobsitelink}</p>
+                </div>
+                </div>`
+                $('#applications').append(appBlock)
             }
         })
     })
+
+    $(document.body).on("click", "#refresh", event => {
+        event.preventDefault();
+        location.reload();
+    });
 });
